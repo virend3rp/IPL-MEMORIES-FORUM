2456dc400c53e885c35e568aac1fb5e01beeb848
Virender Parasariya <parasariya.1@iitj.ac.in>
2025-03-23 03:49:50 +0530

    Removed something imp

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient("mongodb+srv://parasariyav:N1bvVvpSCPzthjPr@ipl-matches.5bdlm.mongodb.net/");
+// Make sure to replace '<Your-Connection-String>' with your actual MongoDB connection string
+const uri = process.env.MONGO_URI; // Assuming you have your MongoDB URI stored in an environment variable named MONGO_URI
+const client = new MongoClient(uri);
 
 const clientPromise = client.connect();
 
